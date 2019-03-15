ac6d2e8933bbab2add4270b08be7b4314728709d
Smoloc <combemorel_dorian@orange.fr>
2019-03-15 13:43:52 +0100

    m

---
+++ js/carou.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+        });
+})
---
+++ js/navcarou.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+        });
+})
